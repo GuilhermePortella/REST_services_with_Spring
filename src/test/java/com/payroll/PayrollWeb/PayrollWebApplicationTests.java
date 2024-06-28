b1b72ecc99615dff465e1cb045444170d27571f5
GuilhermePortella <guilhermeportella2@gmail.com>
2024-06-27 21:07:45 -0300

    Iniciando projeto

---
+++ src/test/java/com/payroll/PayrollWeb/PayrollWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payroll.PayrollWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
