0be1285176e7583ece9cd6b5d69a7cec1fded6f1
GuilhermePortella <guilhermeportella2@gmail.com>
2024-06-29 23:06:59 -0300

    Criando endpoints e verbos

---
+++ src/main/java/com/payroll/PayrollWeb/EmployeeNotFoundException.java
@@ -0,0 +1,12 @@
+package com.payroll.PayrollWeb;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class EmployeeNotFoundException extends RuntimeException {
+    
+    EmployeeNotFoundException(Long id){
+        super("Could not find employee " + id);
+    }
+}
